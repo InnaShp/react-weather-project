a5766f70c5d568f461b27d2a0ec233e5c0eb1a42
InnaShp <107990414+InnaShp@users.noreply.github.com>
2022-10-15 07:38:42 -0700

    Change footer - add netlify link

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ export default function App() {
       <div className="card">
         <Weather defaultCity="Kyiv" />
       </div>
-      <small><a href="https://github.com/InnaShp/react-weather-project" target="_blank" rel="noreferrer" className="git-link">Open-source code</a> , by Inna Shpachynska</small>
+      <footer>
+          This project was coded by Inna Shpachynska and is{" "}
+          <a
+            href="https://github.com/InnaShp/react-weather-project" 
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://legendary-stroopwafel-58bdfa.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
     </div>
   );
 }
