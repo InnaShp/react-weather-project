eef6b1657c52ea086657a6793d7b7b76a861a23c
InnaShp <107990414+InnaShp@users.noreply.github.com>
2022-10-15 04:35:50 -0700

    Add Github link; create basic Weather component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
@@ -19,7 +20,9 @@ export default function App() {
             </div>
           </div>
         </form>
+        <Weather />
       </div>
+      <small><a href="https://github.com/InnaShp/react-weather-project" target="_blank" rel="noreferrer" className="git-link">Open-source code</a> , by Inna Shpachynska</small>
     </div>
   );
 }
